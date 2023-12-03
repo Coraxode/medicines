c81175f277f4b6a9e6c9da00491843f8a046cbd9
Vsevolod Savoliuk <coraxode@gmail.com>
2023-12-03 20:04:18 +0200

    Modificated admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categories, Forms, Manufacturers, CountryOfOrigin, Medicine
 
-# Register your models here.
+
+admin.site.register(Categories)
+admin.site.register(Forms)
+admin.site.register(Manufacturers)
+admin.site.register(CountryOfOrigin)
+admin.site.register(Medicine)
