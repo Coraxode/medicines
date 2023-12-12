cc61eb4484e1931d7c87bf43001f3212a3f874a8
Vsevolod Savoliuk <coraxode@gmail.com>
2023-12-12 13:45:27 +0200

    Rename main page to 'store'

--- main/views.py
+++ main/views.py
@@ -1,25 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Medicine, Categories
 
 
 def index(request):
+    return redirect('store')
+
+
+def store(request):
     medicine_info = []
 
     for medicine in Medicine.objects.all():
         medicine_info.append({
-            # 'id': medicine.id,
             'name': medicine.name,
             'price': medicine.price,
             'photo': medicine.photo,
-            # 'is_prescription_required': medicine.is_prescription_required,
-            # 'date_of_manufacture': medicine.date_of_manufacture,
-            # 'service_life': medicine.service_life,
-            # 'description': medicine.description,
-            # 'quantity': medicine.quantity,
-            'category': medicine.category.first(),
-            'form': medicine.form.first(),
-            'manufacturer': medicine.manufacturer.first(),
-            'country_of_origin': medicine.country_of_origin.first(),
         })
 
     return render(request, 'store/store.html', {'medicines': medicine_info})
