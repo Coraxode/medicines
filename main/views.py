5a32621fdcd97f0fc33f376a5e90caf88a3f67a7
Vsevolod Savoliuk <coraxode@gmail.com>
2023-12-12 12:52:36 +0200

    add the correct display of elements on the main page

--- main/views.py
+++ main/views.py
@@ -8,15 +8,15 @@ def index(request):
     for medicine in Medicine.objects.all():
         print(medicine.category.first())
         medicine_info.append({
-            'id': medicine.id,
+            # 'id': medicine.id,
             'name': medicine.name,
             'price': medicine.price,
             'photo': medicine.photo,
-            'is_prescription_required': medicine.is_prescription_required,
-            'date_of_manufacture': medicine.date_of_manufacture,
-            'service_life': medicine.service_life,
-            'description': medicine.description,
-            'quantity': medicine.quantity,
+            # 'is_prescription_required': medicine.is_prescription_required,
+            # 'date_of_manufacture': medicine.date_of_manufacture,
+            # 'service_life': medicine.service_life,
+            # 'description': medicine.description,
+            # 'quantity': medicine.quantity,
             'category': medicine.category.first(),
             'form': medicine.form.first(),
             'manufacturer': medicine.manufacturer.first(),
