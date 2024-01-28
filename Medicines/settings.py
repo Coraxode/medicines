8b5dbd09870ac0d6d0b8f1d333b4191ad554e226
Vsevolod Savoliuk <coraxode@gmail.com>
2024-01-29 00:33:38 +0200

    Added environment

--- Medicines/settings.py
+++ Medicines/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*4dykcw8@f&lx)_-h4bv)faq5%e+m7j4iw#09&4+fjy@2e@y)0'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(filter(None, os.getenv('ALLOWED_HOSTS', '').split(', ')))
 
 # Application definition
 
